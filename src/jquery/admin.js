5fdbf5e0bf035582da514c6441e16f0a30118ca6
Ann143 <mtelez138@gmail.com>
2021-04-14 12:16:13 +0800

    Admin dashboard modified

--- src/jquery/admin.js
+++ src/jquery/admin.js
@@ -1,6 +1,14 @@
-$('.button-collapse').sideNav();
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-$('.collapsible').collapsible();
-
-$('select').material_select();
-
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
